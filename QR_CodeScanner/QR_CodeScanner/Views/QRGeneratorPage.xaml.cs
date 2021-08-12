003cc0388f5ba2c8b5e4def2ec259ee421f30bc9
KarsaOrlong1981 <69248804+KarsaOrlong1981@users.noreply.github.com>
2021-08-12 16:19:38 +0200

    PinchToZoom Container hinzugefügt

--- QR_CodeScanner/QR_CodeScanner/Views/QRGeneratorPage.xaml.cs
+++ QR_CodeScanner/QR_CodeScanner/Views/QRGeneratorPage.xaml.cs
@@ -9,12 +9,18 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using ZXing;
 using QR_CodeScanner.ViewModel;
+using Xamarin.Forms.Internals;
 
 namespace QR_CodeScanner.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class QRGeneratorPage : ContentPage
     {
+        private double startScale;
+        private double currentScale;
+        private double xOffset;
+        private double yOffset;
+
         [Obsolete]
         public QRGeneratorPage(string qrCodeText,bool isContact)
         {
@@ -22,9 +28,7 @@ namespace QR_CodeScanner.Views
 
             BindingContext = new QRGeneratorViewModel(qrCodeText, isContact);              
         }
-       
-        
-        
-       
+
+      
     }
 }
