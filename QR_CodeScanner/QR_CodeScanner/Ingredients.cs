4f2e1d29b9ec197c7d04b909384ff933b45bb7d5
KarsaOrlong1981 <69248804+KarsaOrlong1981@users.noreply.github.com>
2021-10-06 14:10:01 +0200

    Food Page  still in progress

---
+++ QR_CodeScanner/QR_CodeScanner/Ingredients.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QR_CodeScanner
+{
+    public class Ingredients
+    {
+        public string Ingredient { get; set; }
+        public string Gram { get; set; }
+        public string Unit { get; set; }
+    }
+}
